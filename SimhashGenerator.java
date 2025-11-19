1ee745b5780c7273ee04d101a003a6a53c37c5fd
Sargun777 <sargunrandhawa4317@gmail.com>
2025-11-19 13:09:01 -0500

    Added  line normalization for Simhash(very sensitive )

--- SimhashGenerator.java
+++ SimhashGenerator.java
@@ -12,7 +12,7 @@ public class SimhashGenerator{
             this.CombinedSimhash = contentSimhash ^ contextSimhash;// combines two hashes XOR (^)combines two Simhashes without increasing size which later to be used to find Hamming distance 
         }
     
-    //Used to store a “possible  match with the other file”
+    //Used to store a “possible  match of files”
     public static class Candidate {
         public final int newIndex;//will store index of line in  NEW file might match that old line 
         public final int hammingDistance;//how similar they are
@@ -26,4 +26,20 @@ public class SimhashGenerator{
             return "Candidate{newIndex=" + newIndex + ", distance=" + hammingDistance + "}";
         }
     }
-    }
+        /* We normalize again in Step 3 because Simhash is very sensitive to tiny differences.
+    Even though Step 1 already cleaned the file globally, Simhash needs its own
+          line-level normalization so it can compare lines   detect similarity*/
+        
+    public String normalizeLine(String line) {
+    // if line is empty then it returns an empty string to avoid errors 
+        if (line == null) {
+            return "";
+        }
+     // convert everything to lowercase
+        String normalized = line.toLowerCase(Locale.ROOT);
+        // collapse multiple spaces/tabs into a single space and trim
+        normalized = normalized.replaceAll("\\s+", " ").trim();
+        return normalized;
+    }}
+    
+    
