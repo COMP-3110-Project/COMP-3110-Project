500ad4d1e8b59677351dd74e99f516252218251f
Sargun777 <sargunrandhawa4317@gmail.com>
2025-11-19 12:31:15 -0500

    Added Candidate helper classes

--- SimhashGenerator.java
+++ SimhashGenerator.java
@@ -1,4 +1,4 @@
-public class Simhash{
+public class SimhashGenerator{
     public static class LineSimhash {
         public final int index;
         public final long ContentSimhash;//captures the line itself
@@ -11,5 +11,19 @@ public class Simhash{
             this.ContextSimhash = contextSimhash; 
             this.CombinedSimhash = contentSimhash ^ contextSimhash;// combines two hashes XOR (^)combines two Simhashes without increasing size which later to be used to find Hamming distance 
         }
-    }}
-
+    
+    //Used to store a “possible  match with the other file”
+    public static class Candidate {
+        public final int newIndex;//will store index of line in  NEW file might match that old line 
+        public final int hammingDistance;//how similar they are
+//Creating  a Candidate object for storing the index and similarity distance 
+        public Candidate(int newIndex, int hammingDistance) {
+            this.newIndex = newIndex;
+            this.hammingDistance = hammingDistance;
+        }
+        @Override
+        public String toString() {
+            return "Candidate{newIndex=" + newIndex + ", distance=" + hammingDistance + "}";
+        }
+    }
+    }
