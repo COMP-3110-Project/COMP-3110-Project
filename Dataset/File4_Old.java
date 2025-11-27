8af42a8a2a56a3060c7dd9bf70c743b5dea37f0a
Ifechi Ude <125365641+lemondrops05@users.noreply.github.com>
2025-11-27 18:07:47 +0000

    Dataset: Add File4 Shows deletions

---
+++ Dataset/File4_Old.java
@@ -0,0 +1,12 @@
+public class File4 {
+    private double celsius;
+    public Temperature(double c) {
+        this.celsius = c;
+    }
+    public double toFahrenheit() {
+        return (celsius * 9/5) + 32;
+    }
+    public void print() {
+        System.out.println(celsius);
+    }
+}
