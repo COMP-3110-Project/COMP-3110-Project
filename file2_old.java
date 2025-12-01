ef7766e55d71e605f69f4c6b9a6718e52bc87027
Sargun777 <sargunrandhawa4317@gmail.com>
2025-11-30 20:45:16 -0500

    Create file2_old.java

---
+++ file2_old.java
@@ -0,0 +1,41 @@
+public class file2_old{
+    public static void main(String[] args) {
+        System.out.println("File2 demo starting.");
+        exampleOne();
+        exampleTwo();
+        exampleThree();
+        System.out.println("File2 demo finished.");
+    }
+    private static void exampleOne() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+    private static void exampleTwo() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append("*");
+            System.out.println("Stars: " + sb.toString());
+        }
+    }
+    private static void exampleThree() {
+        int[] data = {3, 1, 4, 1, 5};
+        int max = data[0];
+        int min = data[0];
+        int sum = 0;
+        for (int value : data) {
+            if (value > max) {
+                max = value;
+            }
+            if (value < min) {
+                min = value;
+            }
+            sum += value;
+        }
+        double average = sum / (double) data.length;
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+        System.out.println("Average: " + average);
+    }
+}
