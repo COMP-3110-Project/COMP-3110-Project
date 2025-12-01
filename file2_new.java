1610238bf2299be356cb5a71f673df4b564b741d
Sargun777 <sargunrandhawa4317@gmail.com>
2025-11-30 23:29:16 -0500

    Create file2_new.java

---
+++ file2_new.java
@@ -0,0 +1,45 @@
+public class file2 {
+    private static void exampleThree() {
+        int[] data = {3, 1, 4, 1, 5};
+        int max = data[0];
+        int min = data[0];
+        int sum = 0;
+        for (int value : data) {
+            if (value > max) {
+                max = value;
+            }
+            if (value < min) {
+                min = value;
+            }
+            sum += value;
+        }
+        double average = sum / (double) data.length;
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+        System.out.println("Average: " + average);
+    }
+    private static void exampleOne() {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+    private static void exampleTwo() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            sb.append("*");
+            System.out.println("Stars: " + sb.toString());
+        }
+    }
+    private static void exampleFour() {
+        System.out.println("This is the new method added to File2.");
+    }
+    public static void main(String[] args) {
+        System.out.println("File2 demo starting.");
+        exampleThree();
+        exampleOne();
+        exampleTwo();
+        exampleFour(); // call new method
+        System.out.println("File2 demo finished.");
+    }
+}
