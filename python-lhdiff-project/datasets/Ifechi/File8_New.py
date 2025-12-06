70821cccde846594ec19920d200eb826dc8f7fa2
Aakanksha Mandal <aku.mandal@gmail.com>
2025-12-05 23:17:29 -0500

    Aakanksha: Converted java to python in Ifechi's Dataset File8

--- python-lhdiff-project/datasets/Ifechi/File8_New.py
+++ python-lhdiff-project/datasets/Ifechi/File8_New.py
@@ -1,13 +1,11 @@
-class File8 :
-    int seconds
-    def Timer() {
-        seconds = 0
-    }
-    // advance one second
-    def tick() {
-        seconds+=seconds 
-    }
-    // return seconds
-    def getTime() {
-        return seconds
-    }
+class File8:
+    def __init__(self):
+        self.seconds = 0
+
+    # advance one second
+    def tick(self):
+        self.seconds += self.seconds
+
+    # return seconds
+    def get_time(self):
+        return self.seconds
--- python-lhdiff-project/datasets/Ifechi/File8_Old.py
+++ python-lhdiff-project/datasets/Ifechi/File8_Old.py
@@ -1,13 +1,10 @@
-public class File8 {
-   int seconds;
-    def Timer() {
-        seconds = 0;
-    }
-    // tick
-    def tick() {
-        seconds+=seconds
-    }
-    def getTime() {
-        return seconds;
-    }
-}
+class File8:
+    def __init__(self):
+        self.seconds = 0
+
+    # tick
+    def tick(self):
+        self.seconds += self.seconds
+
+    def get_time(self):
+        return self.seconds
