74ab565b3961be319e825074897ad43099f02c3a
Atefeh <111074334+Calcifer8@users.noreply.github.com>
2025-12-06 20:40:43 -0500

    Update File01_new.py

--- File01_new.py
+++ File01_new.py
@@ -1,8 +1,8 @@
 # File01_New.py
 
 def uottawa_final(points):
-    bonus = points + 10  # Increased bonus
-    return bonus
+    total = points + 10  # Increased bonus
+    return total
 print(uottawa_final(80))
 print("Grade boosted!")
 print("Final grade ready")
