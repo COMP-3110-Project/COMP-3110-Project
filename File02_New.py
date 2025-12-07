139bb4ec47af96a556f106976dd0840eeadc54c1
Atefeh <111074334+Calcifer8@users.noreply.github.com>
2025-12-06 20:51:35 -0500

    Update File02_New.py

--- File02_New.py
+++ File02_New.py
@@ -1,9 +1,9 @@
 # File02_New.py
 
-courses = [
-    "COMP", "MATH", "PHYS"]
+courses = ["COMP", "MATH", "PHYS"]
+
 def mcgill_display():
     for c in courses:
-        print(c.upper())
+        print(c)
 mcgill_display()
 print("McGill 2025 updated")
